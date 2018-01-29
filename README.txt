2a9c63b2941051dc387116a9680c2986cd45169e
Benjamin Koppe <Benjamin.Koppe@gmx.de>
2018-01-29 07:17:00 +0100

    initial commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/tonnerkiller/pen/xpPYmv.
+
+ The pen, once completed, is meant to retrieve a random quote from some free quote APIs and display it. A new quote shall be displayed when a button is clicked.
+There is also social media sharing to Twitter and Hubzilla as an example for a decentralised network. 
+Data is sent by window.open() and a query string, as this is the easiest way to circumvent same-origin policy. Actually like this it is calling server side code and giving parameters, rather than sending data to the server.
+
+This is from the freeCodeCamp "Random Quote Generator" challenge.
---
+++ js/index.js
@@ -0,0 +1,113 @@
+/*
+  (randomizer)
+  
+  getJSON
+     drawbuttons to div
+     updatebuttons
+  
+  document ready
+    call getJSON to set initial site
+    append getJSON to New Quote button
+*/
+
+/* apiArray holding link, quote- and authorkeys of respective JSON, possibility to add more Quote APIs*/
+var apiArray = [{
+    link : "https://talaikis.com/api/quotes/random/" ,
+    quotekey:"quote",
+    authorkey:"author",
+    credit:"Free Random Quotes API by <a href='https://talaikis.com/'>Tadas Talaikis</a>",
+    twitter:"Talaikis"
+  },{
+    link: "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?",
+    quotekey:"quoteText",
+    authorkey:"quoteAuthor",
+    credit:"Forismatic API by <a href='https://alty.co/'>Alty</a>",
+    twitter:"Alterplay"
+  }
+];
+/*--------------------------------------apiArray----------------------------------*/
+
+/*singleton object to keep actual quote-author pair, used throughout the site*/
+var quoteSingleton = {
+  text: "",
+  author: "",
+  credit: "",
+  twitter: "",
+  getTwitter:function(){
+    return twitter;
+  },
+  setTwitter:function(input){
+    twitter=input;
+  },
+  getCredit:function(){
+    return credit;
+  },
+  setCredit:function(input){
+    credit=input;
+  },
+  getText:function(){
+    return text;
+  },
+  setText:function(input){
+    text=input;
+  },
+  getAuthor(){
+    return author;
+  },
+  setAuthor(input){
+    author=input;
+  }
+};
+/*---------------------Singleton Object "quoteSingleton"------------------------*/
+
+
+/*getQuote(apiList,quoteObject) chooses a random API from apiArray, retrieves quote and author from API in JSON form
+and fills data in respective quoteObject properties, also updates html classes quote and author respectively*/
+function getQuote(apiList,quoteObject){
+  var apiObject = apiList[Math.floor(apiArray.length*Math.random())];//get a random api
+  quoteObject.setCredit("Retrieved from: "+apiObject.credit);
+  quoteObject.setTwitter(apiObject.twitter);
+  $(".quotesource").html(quoteObject.getCredit());
+  $.getJSON(apiObject.link).done(function(json){
+    $.each(json,function(key,value){
+        switch(key){
+          case apiObject.quotekey:
+            quoteObject.setText(value);
+            $(".quote").html(value);
+            break;
+          case apiObject.authorkey:
+            quoteObject.setAuthor(value);
+            $(".author").html(value);
+            break;
+        }
+      });
+  });
+};
+/*----------------getQuote(apiList,quoteObject)------------------*/
+
+
+
+
+
+
+
+$("document").ready(function(){
+  getQuote(apiArray,quoteSingleton);
+  $("#tweetbutton").on('click',function(){window.open("https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp"+encodeURIComponent(", author of quotes app challange")+quoteSingleton.twitter+encodeURIComponent(", author of API used")+",tonkllr"+encodeURIComponent(", author of Quotes Machine App")+"&text="+quoteSingleton.getText()+" - "+quoteSingleton.getAuthor())});
+  $("#hubbutton").on('click', function(){
+                     //get hub and channel name
+                     var fullchannel=document.getElementById("channel").value.split("@");
+                     var quote= "[quote="+quoteSingleton.getAuthor()+"]"+quoteSingleton.getText()+"[/quote]"+"<br/>[b]Credit: "+quoteSingleton.getCredit()+"[/b]";
+                     var x = window.open("https://"+fullchannel[1]+"/rpost?&body="+quote+"&title=News from the Quotes Machine&channel="+fullchannel[0]);
+                    
+  //need to forward to actual message or close window
+  });
+  /*$("#gnubutton").on('click',function(){
+    var instance="gnusocial.de";
+    var username="tonnerkiller";
+    window.open("https://"+instance+"/api/statuses/update.xml?f=&text="+quote);
+  });*/
+  $("#newquote").on('click',function(){
+    getQuote(apiArray,quoteSingleton);
+  });
+});
