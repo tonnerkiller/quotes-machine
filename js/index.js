70f24113f14b796faf27a631d4b3d58836d8660f
Benjamin Koppe <Benjamin.Koppe@gmx.de>
2018-01-30 22:24:54 +0100

    css changes

--- js/index.js
+++ js/index.js
@@ -1,15 +1,3 @@
-/*
-  (randomizer)
-  
-  getJSON
-     drawbuttons to div
-     updatebuttons
-  
-  document ready
-    call getJSON to set initial site
-    append getJSON to New Quote button
-*/
-
 /* apiArray holding link, quote- and authorkeys of respective JSON, possibility to add more Quote APIs*/
 var apiArray = [{
     link : "https://talaikis.com/api/quotes/random/" ,
@@ -60,7 +48,6 @@ var quoteSingleton = {
 };
 /*---------------------Singleton Object "quoteSingleton"------------------------*/
 
-
 /*getQuote(apiList,quoteObject) chooses a random API from apiArray, retrieves quote and author from API in JSON form
 and fills data in respective quoteObject properties, also updates html classes quote and author respectively*/
 function getQuote(apiList,quoteObject){
@@ -85,29 +72,17 @@ function getQuote(apiList,quoteObject){
 };
 /*----------------getQuote(apiList,quoteObject)------------------*/
 
-
-
-
-
-
-
 $("document").ready(function(){
   getQuote(apiArray,quoteSingleton);
-  $("#tweetbutton").on('click',function(){window.open("https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp"+encodeURIComponent(", author of quotes app challange")+quoteSingleton.twitter+encodeURIComponent(", author of API used")+",tonkllr"+encodeURIComponent(", author of Quotes Machine App")+"&text="+quoteSingleton.getText()+" - "+quoteSingleton.getAuthor())});
+  $("#tweetbutton").on('click',function(){
+                    window.open("https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp"+encodeURIComponent(", author of quotes app challange")+quoteSingleton.twitter+encodeURIComponent(", author of API used")+",tonkllr"+encodeURIComponent(", author of Quotes Machine App")+"&text="+quoteSingleton.getText()+" - "+quoteSingleton.getAuthor())});
   $("#hubbutton").on('click', function(){
-                     //get hub and channel name
-                     var fullchannel=document.getElementById("channel").value.split("@");
-                     var quote= "[quote="+quoteSingleton.getAuthor()+"]"+quoteSingleton.getText()+"[/quote]"+"<br/>[b]Credit: "+quoteSingleton.getCredit()+"[/b]";
-                     var x = window.open("https://"+fullchannel[1]+"/rpost?&body="+quote+"&title=News from the Quotes Machine&channel="+fullchannel[0]);
-                    
-  //need to forward to actual message or close window
-  });
-  /*$("#gnubutton").on('click',function(){
-    var instance="gnusocial.de";
-    var username="tonnerkiller";
-    window.open("https://"+instance+"/api/statuses/update.xml?f=&text="+quote);
-  });*/
+                    //get hub and channel name
+                    var fullchannel=document.getElementById("channel").value.split("@");
+                    var quote= "[quote="+quoteSingleton.getAuthor()+"]"+quoteSingleton.getText()+"[/quote]"+"<br/>[b]Credit: "+quoteSingleton.getCredit()+"[/b]";
+                    window.open("https://"+fullchannel[1]+"/rpost?&body="+quote+"&title=News from the Quotes Machine&channel="+fullchannel[0]);
+                  });
   $("#newquote").on('click',function(){
-    getQuote(apiArray,quoteSingleton);
+                    getQuote(apiArray,quoteSingleton);
   });
 });
